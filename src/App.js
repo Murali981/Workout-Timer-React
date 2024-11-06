92e85caccdf5d2f007efa0f1c7049bb2c2ee0cde
Murali981 <muraliju981@gmail.com>
2024-11-07 00:45:13 +0530

    Implemented the memo() and useMemo() hook for memoizing the values inorder to prevent the unnecessary wasted renders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Calculator from "./Calculator";
 import ToggleSounds from "./ToggleSounds";
+
+function formatTime(date) {
+  return new Intl.DateTimeFormat("en", {
+    month: "short",
+    year: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  }).format(date);
+}
 
 function App() {
   const [allowSound, setAllowSound] = useState(true);
@@ -9,38 +19,35 @@ function App() {
   // Will be be AM or PM
   const partOfDay = time.slice(-2);
 
-  const workouts = [
-    {
-      name: "Full-body workout",
-      numExercises: partOfDay === "AM" ? 9 : 8,
-    },
-    {
-      name: "Arms + Legs",
-      numExercises: 6,
-    },
-    {
-      name: "Arms only",
-      numExercises: 3,
-    },
-    {
-      name: "Legs only",
-      numExercises: 4,
-    },
-    {
-      name: "Core only",
-      numExercises: partOfDay === "AM" ? 5 : 4,
-    },
-  ];
-
-  function formatTime(date) {
-    return new Intl.DateTimeFormat("en", {
-      month: "short",
-      year: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    }).format(date);
-  }
+  const workouts = useMemo(() => {
+    return [
+      // Since this workouts prop that we are passing into a Calculator component is an array of objects
+      // which is essentially just a javascript object with a key and a value pair. So this array gets recreated on every single
+      // render And so therefore we have to memoize this array inorder to memoizing of the Calculator component to work. So we are
+      // using a useMemo() hook as this is a value to store in the cache but not a function and if it is a function to memoize
+      // then we will use the useCallback() hook.
+      {
+        name: "Full-body workout",
+        numExercises: partOfDay === "AM" ? 9 : 8,
+      },
+      {
+        name: "Arms + Legs",
+        numExercises: 6,
+      },
+      {
+        name: "Arms only",
+        numExercises: 3,
+      },
+      {
+        name: "Legs only",
+        numExercises: 4,
+      },
+      {
+        name: "Core only",
+        numExercises: partOfDay === "AM" ? 5 : 4,
+      },
+    ];
+  }, [partOfDay]);
 
   useEffect(function () {
     const id = setInterval(function () {
--- src/ToggleSounds.js
+++ src/ToggleSounds.js
@@ -1,3 +1,13 @@
+import { memo } from "react";
+
+/* HANDLING THE PERFORMANCE ISSUE ?
+
+   If the children components are re-rendering just because the parent component is re-rendering then the solution is to
+   memoize those children components. Here in the below we are memoizing the ToggleSounds component below. As we are exporting
+   the below ToggleSounds component we can memoize using the memo hook directly in the export statement as below where we are
+   exporting the result like this "export default memo(ToggleSounds)"
+*/
+
 function ToggleSounds({ allowSound, setAllowSound }) {
   return (
     <button
@@ -9,4 +19,6 @@ function ToggleSounds({ allowSound, setAllowSound }) {
   );
 }
 
-export default ToggleSounds;
+export default memo(ToggleSounds); // Memoizing the component As we are exporting
+// the below ToggleSounds component we can memoize using the memo hook directly in the export statement as below where we are
+// exporting the result like this "export default memo(ToggleSounds)"
